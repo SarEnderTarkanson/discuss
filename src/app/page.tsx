11dbb383ed0de0b33a0cd311df6e1b324f201ddf
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-04-19 20:41:32 +0200

    created the singIn and signOut server actions / wrapped the provider with the react context sessionProvider / implemented the actions inside the client component / successfully read the user data both on the server component and client component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
 import { Button } from "@nextui-org/react";
+import * as actions from "@/actions";
+import { auth } from "@/auth";
+import Profile from "@/components/profile";
 
+export default async function Home() {
+  const session = await auth();
 
-export default function Home() {
   return (
     <div>
-      <Button>Click Me!!!</Button>
+      <form action={actions.signIn}>
+        <Button type="submit">Sign in</Button>
+      </form>
+      <form action={actions.signOut}>
+        <Button type="submit">Sign out</Button>
+      </form>
+
+      {session?.user ? (
+        <div>{JSON.stringify(session.user)}</div>
+      ) : (
+        <div>Signed Out</div>
+      )}
+      <Profile />
     </div>
   );
 }
