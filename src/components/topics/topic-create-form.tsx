dd0b64d0473b55795c2dc9056a780a78a609dede
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-04-26 02:08:21 +0200

    created a FormButton

--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -9,11 +9,13 @@ import {
   Popover,
   PopoverTrigger,
   PopoverContent,
+  Form,
 } from "@nextui-org/react";
 import * as actions from "@/actions";
+import FormButton from "@/components/common/form-button";
 
 export default function TopicCreateForm() {
-  const [formState, action] = useActionState(actions.createTopic, {
+  const [formState, action, isPending] = useActionState(actions.createTopic, {
     errors: {},
   });
 
@@ -31,7 +33,7 @@ export default function TopicCreateForm() {
         <Button color="primary">Create a Topic</Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form onSubmit={handleSubmit} noValidate>
+        <Form onSubmit={handleSubmit}>
           <div className="flex flex-col gap-4 p-4 w-80">
             <h3 className="text-lg">Create a Topic</h3>
             <Input
@@ -50,14 +52,16 @@ export default function TopicCreateForm() {
               isInvalid={!!formState.errors.description}
               errorMessage={formState.errors.description?.join(", ")}
             />
+
             {formState.errors._form ? (
               <div className="rounded p-2 bg-red-200 border border-red-400">
                 {formState.errors._form?.join(", ")}
               </div>
             ) : null}
-            <Button type="submit">Submit</Button>
+
+            <FormButton isLoading={isPending}>Save</FormButton>
           </div>
-        </form>
+        </Form>
       </PopoverContent>
     </Popover>
   );
