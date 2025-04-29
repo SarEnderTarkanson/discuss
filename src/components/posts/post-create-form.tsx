57ad3eff83371c73f6b4dd7f78ed6c74a32036fd
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-04-29 23:41:15 +0200

    applied validation for the post creation with the help of create-post server action

--- src/components/posts/post-create-form.tsx
+++ src/components/posts/post-create-form.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useActionState } from "react";
+
 import {
   Input,
   Button,
@@ -10,32 +11,41 @@ import {
   PopoverContent,
 } from "@nextui-org/react";
 import * as actions from "@/actions";
-import FormButton from "../common/form-button";
+import FormButton from "@/components/common/form-button";
 
 export default function PostCreateForm() {
+  const [formState, action, isPending] = useActionState(actions.createPost, {
+    errors: {},
+  });
+
   return (
     <Popover placement="left">
       <PopoverTrigger>
         <Button color="primary">Create a Post</Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form action="">
+        <form action={action}>
           <div className="flex flex-col gap-4 p-4 w-80">
-            <h3 className="text-lg">Create Post</h3>
+            <h3 className="text-lg">Create a Post</h3>
+
             <Input
+              isInvalid={!!formState.errors.title}
+              errorMessage={formState.errors.title?.join(", ")}
               name="title"
               label="Title"
               labelPlacement="outside"
               placeholder="Title"
             />
-            <Input
+            <Textarea
+              isInvalid={!!formState.errors.content}
+              errorMessage={formState.errors.content?.join(", ")}
               name="content"
               label="Content"
               labelPlacement="outside"
               placeholder="Content"
             />
 
-            <FormButton>Create Post</FormButton>
+            <FormButton isLoading={isPending}>Create Post</FormButton>
           </div>
         </form>
       </PopoverContent>
