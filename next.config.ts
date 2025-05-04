dab9bc1ba407abcc2272268b6ebaacc2c04b14d7
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-05-04 15:44:10 +0200

    merged a few files

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
   },
-
 };
 
 export default nextConfig;
