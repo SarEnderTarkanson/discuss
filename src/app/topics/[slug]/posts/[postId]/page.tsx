77d3c79d5db2a68166549750506406b3a77b34d7
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-05-18 02:26:41 +0200

    completed the functionality to add/display comments and to reply them

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import PostShow from "@/components/posts/post-show";
 import CommentList from "@/components/comments/comment-list";
+import { fetchCommentsByPostId } from "@/db/queries/comments";
 import CommentCreateForm from "@/components/comments/comment-create-form";
 import paths from "@/paths";
 
@@ -19,9 +20,9 @@ export default async function PostShowPage({ params }: PostShowPageProps) {
       <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
         {"< "}Back to {slug}
       </Link>
-      <PostShow postId={postId}/>
-      {/* <CommentCreateForm postId={postId} startOpen /> */}
-      {/* <CommentList comments={comments} /> */}
+      <PostShow postId={postId} />
+      <CommentCreateForm postId={postId} startOpen />
+      <CommentList fetchData={() => fetchCommentsByPostId(postId)} />
     </div>
   );
 }
